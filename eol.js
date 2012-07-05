51b11bfcaa8d8c55fa8c28b883a93cf71c0cf506
Danny Su <dannysu@live.com>
2012-07-05 00:17:15 -0400

    Revert "Revert memory reduction attempt"
    This reverts commit 91056209a3e780d0ebc050e7f42fe06ff721976b.

--- eol.js
+++ eol.js
@@ -186,6 +186,7 @@
                 self.lives.push(life);
             });
 
+            self.onScroll(self.layout.position);
             self.padWithLoadingCells();
         }
 
@@ -198,6 +199,7 @@
             self.layout.resize(width, height);
             self.search_term = search_term;
             self.collection_id = collection_id;
+            self.onScroll(self.layout.position);
             self.padWithLoadingCells();
         }
 
@@ -217,6 +219,7 @@
                 });
             });
 
+            self.onScroll(self.layout.position);
             self.padWithLoadingCells();
         }
 
@@ -271,9 +274,29 @@
             self.collect(self.selectedItem(), null, 'remove');
         }
 
+        self.viewableRow = ko.observable(-1);
+        self.viewables = ko.observableArray([]);
+
         self.pageOffset = 0;
         self.onScroll = function(position) {
             self.layout.updatePosition(position);
+
+            var currentRow = self.layout.getCurrentRow();
+
+            var numRowsOnScreen = self.layout.getNumRows();
+            var numColumnsOnScreen = self.layout.getNumColumns();
+
+            var startIndex = currentRow * numColumnsOnScreen;
+            var relevantItems = self.lives().slice(startIndex, startIndex + (numRowsOnScreen + 2) * numColumnsOnScreen);
+
+            if (relevantItems.length != self.viewables().length || currentRow != self.viewableRow()) {
+                self.viewables.removeAll();
+                self.viewableRow(currentRow);
+
+                for (var i = 0; i < relevantItems.length; i++) {
+                    self.viewables.push(relevantItems[i]);
+                }
+            }
 
             // Only save progress for show all page
             if (self.collection_id == null && self.search_term == "*") {
