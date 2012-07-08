a25547076172bfded4276cfb6d00ab842dd57d11
Danny Su <dannysu@live.com>
2012-07-07 23:44:25 -0400

    Fix cells not showing after flicker fix

--- eol.js
+++ eol.js
@@ -307,17 +307,14 @@
                     }
                 }
                 else {
-                    var diff = relevantItems.length - self.viewables().length;
-                    for (var i = 0; i < diff; i++) {
-                        self.viewables.push(new LifeViewModel('', '', '', ''));
-                    }
-
-                    var left = [];
+                    // Initialize array indicating which cell is free to be reused
                     var free = [];
-                    for (var i = 0; i < relevantItems.length; i++) {
+                    for (var i = 0; i < self.viewables().length; i++) {
                         free.push(true);
                     }
 
+                    // Go through relevant items to be shown and find reusable cell to replace
+                    var left = [];
                     for (var i = 0; i < relevantItems.length; i++) {
                         var found = false;
                         for (var j = 0; j < self.viewables().length; j++) {
@@ -336,6 +333,14 @@
                         }
                     }
 
+                    // If there are any relevant items left over due to insufficient viewables to replace,
+                    // then add new ones.
+                    var diff = relevantItems.length - self.viewables().length;
+                    for (var i = 0; i < diff; i++) {
+                        self.viewables.push(left.pop());
+                    }
+
+                    // Go through rest of relevant items that don't already have reusable cell
                     for (var i = 0; i < left.length; i++) {
                         for (var j = 0; j < free.length; j++) {
                             if (free[j]) {
